d65f6e9aaa6f4052cda70e6f9afe6eace978a100
04stefke <jovanovics399@yahoo.com>
2025-01-14 14:36:09 +0000

    started styling the form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import check from '/assets/icon-check.svg'
 const Form = () => {
-  return (
-	<div>Form</div>
-  )
-}
+	return (
+		<form className="w-full flex items-center bg-dark-backgroundColor2 rounded-md">
+			<div className="w-full bg-transparent flex items-center px-2 ">
+				<img src={check} alt="" className="bg-gradient-checklist rounded-full px-[4px] py-[5px] cursor-pointer border-dark-veryDarkGrayishBlue2 border-1 "  />
+				<input
+					type="text"
+					className="w-full py-4 px-4 outline-none bg-transparent text-gray-600 "
+					placeholder="Create a new todo..."
+				/>
+			</div>
+		</form>
+	);
+};
 
-export default Form
+export default Form;
