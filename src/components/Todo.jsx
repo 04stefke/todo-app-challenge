dbd981ead3bad87fc2e44fde38f3a2d8d2dd8703
04stefke <jovanovics399@yahoo.com>
2025-01-16 12:13:02 +0000

    styled todolist and styling todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Todo = () => {
   return (
-	<div>Todo</div>
+	<div className='px-2 py-2 border-b border-gray-500 cursor-pointer text-white flex items-center gap-3 w-full'>
+    
+  </div>
   )
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Todo from './Todo'
 const TodoList = () => {
   return (
-	<div className='bg-dark-backgroundColor2 rounded-md'>
+	<div className='bg-dark-backgroundColor2 rounded-md flex flex-col justify-center'>
     <Todo/>
     <Todo/>
     <Todo/>
     <Todo/>
     <Todo/>
-    
+
   </div>
   )
 }
