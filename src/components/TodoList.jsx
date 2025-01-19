3709c8be2ebf0619266b9e68d329823ad1d62337
04stefke <jovanovics399@yahoo.com>
2025-01-19 15:52:47 +0000

    styled the todolist component so that the edit stay always on the bottom even if there are fewer todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,12 +3,11 @@ import Todo from "./Todo";
 import Edit from "./Edit";
 const TodoList = () => {
 	return (
-		<div className="bg-bg2 rounded-md flex flex-col shadow-2xl min-h-[200px] ">
-			<Todo />
-
+		<div className="bg-bg2 rounded-md flex flex-col shadow-2xl min-h-[300px] ">
 			<div className="h-full w-full ">
-				<Edit />
+				<Todo />
 			</div>
+			<Edit />
 		</div>
 	);
 };
