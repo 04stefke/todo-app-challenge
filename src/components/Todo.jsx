31c16c4ac25fbf088537840d265ce846378d1b86
04stefke <jovanovics399@yahoo.com>
2025-01-21 15:13:15 +0000

    called the task prop in todo component, and rendering the tasks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import tick from "../../public/assets/icon-check.svg";
 import cross from "../../public/assets/icon-cross.svg";
-const Todo = () => {
+const Todo = ({task}) => {
 	return (
 		<div className="px-4 py-4 border-b border-borderB  text-lightGray  flex items-center justify-between  gap-3 w-full">
 			<div className="flex items-center gap-3">
 				<div className=" hover:border-light-gradient-color border rounded-full px-1 py-[5px] bg-gradient-checklist">
 					<img src={tick} alt="" className="cursor-pointer" />
 				</div>
-				<p className="cursor-pointer">tasks to do</p>
+				<p className="cursor-pointer">{task.task}</p>
 			</div>
 			<img src={cross} alt="" className="justify-self-end cursor-pointer" />
 		</div>
