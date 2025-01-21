4dd3bd1488e46c02530793c6703ab72bc2dcc617
04stefke <jovanovics399@yahoo.com>
2025-01-21 15:30:15 +0000

    created a class for completed tasks, and added it to the classname if it meets the criteria

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import tick from "../../public/assets/icon-check.svg";
 import cross from "../../public/assets/icon-cross.svg";
-const Todo = ({task}) => {
+const Todo = ({ task }) => {
 	return (
 		<div className="px-4 py-4 border-b border-borderB  text-lightGray  flex items-center justify-between  gap-3 w-full">
 			<div className="flex items-center gap-3">
-				<div className=" hover:border-light-gradient-color border rounded-full px-1 py-[5px] bg-gradient-checklist">
-					<img src={tick} alt="" className="cursor-pointer" />
+				<div
+					className={`cursor-pointer h-8 w-8 rounded-full border-[1px] border-borderB hover:border-brightBlue flex items-center justify-center ${
+						task.completed ? "completedTaskTick" : ""
+					}`}
+				>
+					<i className="bx bx-check text-bg2 text-lg px-2 py-3"></i>
 				</div>
-				<p className="cursor-pointer">{task.task}</p>
+				<p
+					className={`cursor-pointer ${task.completed ? "completedTask" : ""}`}
+				>
+					{task.task}
+				</p>
 			</div>
 			<img src={cross} alt="" className="justify-self-end cursor-pointer" />
 		</div>
